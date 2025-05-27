3a8ad498ee3588124d8997effa593da7c405fd02
Ben Chen <chenbenjamin2@gmail.com>
2025-05-26 23:01:23 -0700

    clippy

--- src/macros.rs
+++ src/macros.rs
@@ -25,7 +25,7 @@ macro_rules! tensor {
             panic!("Data length {} does not match product of dimensions {}", data.len(), expected_len);
         }
 
-        crate::tensor::Tensor::new(&dims, crate::tensor::Storage::new(data.to_vec()), false)
+        $crate::tensor::Tensor::new(&dims, crate::tensor::Storage::new(data.to_vec()), false)
     }};
 }
 
